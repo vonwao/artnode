70fee4ce8cc0238c5bdf8f327578fa0574bd8862
Otto <vonwao@gmail.com>
2023-12-05 20:03:12 -0500

    improve page

--- pages/nfts.js
+++ pages/nfts.js
@@ -2,45 +2,33 @@ import React, { useEffect, useState } from 'react';
 import { render } from '../tiny-dinos/render';
 
 const HomePage = () => {
-  const [tokenId, setTokenId] = useState('');
+  const [tokenId, setTokenId] = useState(1);
   const [svgContent, setSvgContent] = useState('');
 
   useEffect(() => {
-    const svg = render(1); // Use the appropriate token ID
-    // console.log('SVG returned:', svg);
-    setSvgContent(svg);
-  }, []);
-
-  const renderToken = () => {
-    console.log(`Attempting to render token ID: ${tokenId}`);
-    const svg = render(tokenId);
-    // console.log('SVG returned:', svg);
-    setSvgContent(svg);
-  };
-
+    setSvgContent(render(tokenId));
+  }, [tokenId]);
 
   const handleTokenChange = (change) => {
     setTokenId(prev => Math.max(1, prev + change));
   };
 
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: svgContent }} style={{ border: '1px solid black', width: '200px', height: '200px' }} />
-      <div>Token ID: {tokenId}</div>
-
-      <div>
-        <button onClick={() => handleTokenChange(-1)}>Back</button>
-        <button onClick={() => handleTokenChange(1)}>Next</button>
-
+    <div style={{ padding: '10px' }}>
+      <div dangerouslySetInnerHTML={{ __html: svgContent }} style={{ border: '1px solid black', width: '200px', height: '200px', marginBottom: '10px' }} />
+      <div style={{ marginBottom: '10px' }}>Token ID: {tokenId}</div>
+      <div style={{ marginBottom: '10px' }}>
+        <button onClick={() => handleTokenChange(-1)}>&lt;</button>
+        <button onClick={() => handleTokenChange(1)}>&gt;</button>
       </div>
-
       <div>
         <input
           type="number"
           value={tokenId}
           onChange={e => setTokenId(Number(e.target.value))}
+          style={{ width: '80px', marginRight: '10px' }}
         />
-        <button onClick={() => renderToken(tokenId)}>Go</button>
+        <button onClick={() => handleTokenChange(0)}>Go</button>
       </div>
     </div>
   );
