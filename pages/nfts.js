e15af9aada890e147d1139b8793a0e17f7f60445
Otto <vonwao@gmail.com>
2023-12-05 20:12:20 -0500

    add comment requests for chatGPT

--- pages/nfts.js
+++ pages/nfts.js
@@ -11,17 +11,23 @@ const HomePage = () => {
 
   const handleTokenChange = (change) => {
     setTokenId(prev => Math.max(1, prev + change));
+    // why is setSvgContent not needed here?
   };
 
   return (
     <div style={{ padding: '10px' }}>
       <div dangerouslySetInnerHTML={{ __html: svgContent }} style={{ border: '1px solid black', width: '200px', height: '200px', marginBottom: '10px' }} />
-      <div style={{ marginBottom: '10px' }}>Token ID: {tokenId}</div>
       <div style={{ marginBottom: '10px' }}>
+        {/* use flexbox, two buttons on the right, Token ID on left  */}
+        {/* also enable using keyboard arrows to navigate left/right */}
+        Token ID: {tokenId}
         <button onClick={() => handleTokenChange(-1)}>&lt;</button>
         <button onClick={() => handleTokenChange(1)}>&gt;</button>
+        </div>
+      <div style={{ marginBottom: '10px' }}>
       </div>
       <div>
+        {/* input should be blank to start, and should be reset blank after pressing go, state needs to be indepent */}
         <input
           type="number"
           value={tokenId}
@@ -33,5 +39,7 @@ const HomePage = () => {
     </div>
   );
 };
+
+//can we scale the whole page to be relative to the window size?  So larger window means larger buttons (up to a point)
 
 export default HomePage;
