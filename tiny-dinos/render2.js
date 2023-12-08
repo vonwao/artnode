89b2883b7d51dabcda6779f926c6cb7c1fb6986c
Otto <vonwao@gmail.com>
2023-12-08 15:19:29 -0500

    work

--- tiny-dinos/render2.js
+++ tiny-dinos/render2.js
@@ -1,40 +1,45 @@
-import art from './art';
-import { augmentObject } from './augment-props';
-import traits from './traits';
-import background from './layers/background';
-import dino from './layers/dino';
+import art from "./art";
+import { augmentObject } from "./augment-props";
+import traits from "./traits";
+import background from "./layers/background";
+import dino from "./layers/dino";
 
 // Utility function to merge all layers into a single object
-function mergeLayers(trait) {
-  const merged = {};
-  // Assuming each layer in 'trait' follows the structure { art, colors, offset }
-  Object.keys(trait).forEach(key => {
-    if (trait[key].art) {
-      merged[key] = {
-        art: trait[key].art,
-        colors: trait[key].colors || null,
-        offset: trait[key].offset || { x: 0, y: 0 }
-      };
-    }
-  });
-  return merged;
-}
+// function mergeLayers(trait) {
+//   const merged = {};
+//   // Assuming each layer in 'trait' follows the structure { art, colors, offset }
+//   Object.keys(trait).forEach(key => {
+//     if (trait[key].art) {
+//       merged[key] = {
+//         art: trait[key].art,
+//         colors: trait[key].colors || null,
+//         offset: trait[key].offset || { x: 0, y: 0 }
+//       };
+//     }
+//   });
+//   return merged;
+// }
 
-function renderLayer(layer) {
+function renderLayer(layer, traitValue) {
   if (!layer.art) {
-    console.warn('No art provided for layer');
+    console.warn("No art provided for layer");
     return [];
   }
 
   let pixels = [];
-  const offsetX = layer.offset.x, offsetY = layer.offset.y;
+  const offsetX = layer.offset && layer.offset.x || 0,
+    offsetY = layer.offset && layer.offset.y || 0;
 
-  const rows = layer.art.trim().split('\n');
+    console.log('OFFSET', offsetX, offsetY);
+    console.log('ART', layer.art);
+  const rows = layer.art.trim().split("\n");
   rows.forEach((row, rowIndex) => {
     row = row.trim();
     for (let x = 0; x < row.length; x++) {
-      if (row[x] === '1') {
-        const color = layer.colors ? layer.colors[rowIndex % layer.colors.length] : '#000000';
+      if (row[x] === "1") {
+        const color = layer.colors
+          ? layer.colors[rowIndex % layer.colors.length]
+          : "#000000";
         pixels.push({ x: x + offsetX, y: rowIndex + offsetY, color });
       }
     }
@@ -47,23 +52,35 @@ function generateSvgFromPixels(pixelSets, pixelSize = 20) {
   const svgWidth = 16 * pixelSize;
   const svgHeight = 16 * pixelSize;
 
-  const svgLayers = pixelSets.map(pixels => pixels.map(pixel => 
-    `<rect x="${pixel.x * pixelSize}" y="${pixel.y * pixelSize}" width="${pixelSize}" height="${pixelSize}" fill="${pixel.color}" />`
-  ).join('')).join('');
+  const svgLayers = pixelSets
+    .map((pixels) =>
+      pixels
+        .map(
+          (pixel) =>
+            `<rect x="${pixel.x * pixelSize}" y="${
+              pixel.y * pixelSize
+            }" width="${pixelSize}" height="${pixelSize}" fill="${
+              pixel.color
+            }" />`
+        )
+        .join("")
+    )
+    .join("");
 
   return `<svg width="${svgWidth}" height="${svgHeight}" xmlns="http://www.w3.org/2000/svg">${svgLayers}</svg>`;
 }
 
 export function render(tokenId) {
-  let trait = traits.find(trait => trait.tokenId === tokenId);
+  let trait = traits.find((trait) => trait.tokenId === tokenId);
   trait = augmentObject(trait);
 
   // Merge all layers
-  const allLayers = {...background, ...dino}
-//   const mergedLayers = mergeLayers();
-  console.log('Merged Layers:', Object.keys(allLayers)); // Sanity check
+  const allLayers = { ...background, ...dino };
+  //   const mergedLayers = mergeLayers();
+  console.log("Merged Layers:", Object.keys(allLayers)); // Sanity check
 
   // Render each layer
-  const pixelSets = Object.values(allLayers).map(layer => renderLayer(layer));
+  const pixelSets = Object.values(allLayers).map((layer) => renderLayer(layer));
+  console.log("Pixel Sets:", pixelSets); // Sanity check
   return [trait, generateSvgFromPixels(pixelSets)];
 }
