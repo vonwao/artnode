615cde2d08e1ededb3d429dfbcc8f6af083de80d
Otto <vonwao@gmail.com>
2023-12-08 16:21:29 -0500

    work on new render func

--- tiny-dinos/render2.js
+++ tiny-dinos/render2.js
@@ -1,26 +1,22 @@
-import art from "./art";
 import { augmentObject } from "./augment-props";
 import traits from "./traits";
 import background from "./layers/background";
 import dino from "./layers/dino";
 
-// Utility function to merge all layers into a single object
-// function mergeLayers(trait) {
-//   const merged = {};
-//   // Assuming each layer in 'trait' follows the structure { art, colors, offset }
-//   Object.keys(trait).forEach(key => {
-//     if (trait[key].art) {
-//       merged[key] = {
-//         art: trait[key].art,
-//         colors: trait[key].colors || null,
-//         offset: trait[key].offset || { x: 0, y: 0 }
-//       };
-//     }
-//   });
-//   return merged;
+// layer schema
+// mote string in colors is a hex color
+// layers = {
+//   [layer_key] : {
+//       art: {[trait_value]: string}
+//       colors: {[trait_value]: [string]}
+//       offset: string
+//   }
 // }
 
 function renderLayer(key, traitValue, layer) {
+  if (key === "background") {
+    traitValue = traitValue + ".png";
+  }
   console.log("RENDERING LAYER", key, traitValue, layer);
   if (!layer.art) {
     console.warn("2 No art provided for layer");
@@ -35,9 +31,11 @@ function renderLayer(key, traitValue, layer) {
   const offsetX = (layer.offset && layer.offset.x) || 0,
     offsetY = (layer.offset && layer.offset.y) || 0;
 
+  const art = layer.art[traitValue];
+  console.log("-----ART", art);
   // console.log("OFFSET", offsetX, offsetY);
   // console.log("ART", layer.art);
-  const rows = layer.art[traitValue].trim().split("\n");
+  const rows = art.trim().split("\n");
   rows.forEach((row, rowIndex) => {
     row = row.trim();
     for (let x = 0; x < row.length; x++) {
@@ -96,16 +94,40 @@ function getObjectStructure(obj) {
   return processObject(obj);
 }
 
+function normalizeLayers(layers) {
+  const normalized = {};
+  Object.keys(layers).forEach((layerKey) => {
+    const layer = layers[layerKey];
+
+    // Normalize 'art'
+    normalized[layerKey] = {
+      ...layer,
+      art: typeof layer.art === "string" ? { default: layer.art } : layer.art,
+    };
+
+    // Normalize 'colors'
+    normalized[layerKey].colors = {};
+    Object.keys(layer.colors).forEach((traitKey) => {
+      const colorValue = layer.colors[traitKey];
+      // If the color value is a string, convert it to an array
+      normalized[layerKey].colors[traitKey] =
+        typeof colorValue === "string" ? [colorValue] : colorValue;
+    });
+  });
+  return normalized;
+}
+
 export function render(tokenId) {
   let trait = traits.find((trait) => trait.tokenId === tokenId);
   trait = augmentObject(trait);
 
   // Merge all layers
-  const allLayers = { ...background, ...dino };
+  let allLayers = { ...background, ...dino };
+  allLayers = normalizeLayers(allLayers);
 
   const struct = getObjectStructure(allLayers);
   console.log("Object Structure:", struct); // Sanity check
-  
+
   //   const mergedLayers = mergeLayers();
   console.log("Merged Layers:", Object.keys(allLayers)); // Sanity check
 
